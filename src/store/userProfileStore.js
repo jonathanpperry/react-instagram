bd5f35d6c1fe692186a3b5017f3aac264c871fd4
Jonathan Perry <jpper@umich.edu>
2023-12-08 09:22:52 -0600

    Implemented create post

--- src/store/userProfileStore.js
+++ src/store/userProfileStore.js
@@ -3,7 +3,14 @@ import { create } from "zustand";
 const useUserProfileStore = create((set) => ({
   userProfile: null,
   setUserProfile: (userProfile) => set({ userProfile }),
-  // TODO: addPost
+  // This is used to update the number of posts in the profile page
+  addPost: (post) =>
+    set((state) => ({
+      userProfile: {
+        ...state.userProfile,
+        posts: [post.id, ...state.userProfile.posts],
+      },
+    })),
 }));
 
 export default useUserProfileStore;
